cburgmer/pdfserver

commit 25629539d4bec796691b32c32046d4a452565e57

Implement asynchronous builds for the Google App Engine
- Include all static files in build
- Honour task_result.available and return 404
- Don't pickle all input files on asynchronous call but pass db keys

pdfserver/tasks.py
@@ -3,6 +3,7 @@ __all__ = ['handle_pdfs_task', 'TaskRevokedError', 'NotRegistered']
 from pdfserver.util import handle_pdfs
 from pdfserver import app
 task = __import__(app.config['TASK_HANDLER'], fromlist='task').task
+Upload = __import__(app.config['MODELS'], fromlist='Upload').Upload
 
 if app.config['TASK_HANDLER'] == 'celery.decorators':
     from celery.exceptions import TaskRevokedError, NotRegistered
@@ -12,10 +13,15 @@ elif app.config['TASK_HANDLER'] == 'pdfserver.faketask':
     class TaskRevokedError(Exception):
         pass
 
+elif app.config['TASK_HANDLER'] == 'pdfserver.gaetask':
+    from pdfserver.gaetask import NotRegistered
+    class TaskRevokedError(Exception):
+        pass
 
 @task
-def handle_pdfs_task(files_handles, page_range_text=None, pages_sheet=1,
+def handle_pdfs_task(file_ids, page_range_text=None, pages_sheet=1,
                      rotate=0, overlay=None):
+    files_handles = Upload.get_for_ids(file_ids)
     output = handle_pdfs(files_handles, page_range_text=page_range_text,
                          pages_sheet=pages_sheet, rotate=rotate,
                          overlay=overlay)

MANIFEST.in
@@ -1,9 +1,7 @@
 include pdfserver/translations/*/*/messages.po
 include pdfserver/translations/*/*/messages.mo
 include pdfserver/templates/*.html
-include pdfserver/static/js/*.js
-include pdfserver/static/css/images/*
-include pdfserver/static/css/*.css
+include pdfserver/static/*
 include *.py
 include pdfserver.cgi
 include babel.cfg

app.yaml
@@ -4,5 +4,8 @@ runtime: python
 api_version: 1
 
 handlers:
+- url: /_ah/queue/deferred
+  script: $PYTHON_LIB/google/appengine/ext/deferred/handler.py
+  login: admin
 - url: .*
   script: appengine.py

appengine.py
@@ -2,5 +2,6 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from pdfserver import app
 
 app.config['MODELS'] = 'pdfserver.gaemodels'
+app.config['TASK_HANDLER'] = 'pdfserver.gaetask'
 app.config['SECRET_KEY'] =  # set this to a random string
 run_wsgi_app(app)

pdfserver/faketask.py
@@ -1,6 +1,6 @@
 """
 Fake task handler that emulates celery's task handling.
-Results are processed synchronous and are stored in a database table for later
+Results are processed synchronously and are stored in a database table for later
 retrieval.
 """
 
@@ -96,8 +96,9 @@ class FakeAsyncResult(object):
 
             if task_result is None:
                 raise NotRegistered()
-            #if not task_result.available:
-                # TODO raise ...
+            if not task_result.available:
+                # TODO raise a better Exception
+                raise NotRegistered()
 
             self._task_result = task_result
 

pdfserver/gaemodels.py
@@ -78,7 +78,7 @@ class Upload(db.Model):
                                                                 MAX_BLOB_SIZE)))
             blob.put()
         if len(upload.unsaved_content.read(1)) > 0:
-            raise ValueError("Uploaded file to big")
+            raise ValueError("Uploaded file too big")
 
     def store_file(self, file):
         self.filename = file.filename

pdfserver/gaetask.py
@@ -1,0 +1,270 @@
+"""
+Task handler for the Google App Engine that emulates celery's task handling.
+Results are processed asynchronously and are stored in a database table for
+later retrieval.
+"""
+
+__all__ = ["NotRegistered", "task"]
+
+import sys
+from StringIO import StringIO
+from datetime import datetime, timedelta
+import pickle
+from functools import wraps
+
+from google.appengine.ext import deferred
+from google.appengine.ext import db
+
+from pdfserver import app
+from pdfserver.gaemodels import USE_1MB_WORKAROUND, MAX_BLOB_SIZE
+
+class TaskResult(db.Model):
+    result_blob = db.BlobProperty()
+    success = db.BooleanProperty()
+    created = db.DateTimeProperty(auto_now=True)
+    available = db.BooleanProperty(default=True)
+    pending = db.BooleanProperty(default=True)
+
+    def __repr__(self):
+        return '<TaskResult %r>' % self.task_id
+
+    @property
+    def task_id(self):
+        return self.key().id()
+
+    @classmethod
+    def get_for_task_id(cls, task_id):
+        return cls.get_by_id(task_id)
+
+    @classmethod
+    def add(cls, task_result):
+        key = task_result.put()
+        if USE_1MB_WORKAROUND:
+            try:
+                cls.store_unsaved_content(task_result)
+            except ValueError:
+                cls.delete(task_result)
+                raise
+
+    @classmethod
+    def clean(cls):
+        """Clean up old results."""
+        oldest_keep_datetime = datetime.utcnow() - timedelta(
+                                    seconds=app.config['TASK_RESULT_EXPIRES'])
+        for task_result in cls.all().filter('created <', oldest_keep_datetime):
+            TaskResult.delete(task_result)
+
+    @classmethod
+    def delete(cls, task_result):
+        db.delete([task_result])
+        if USE_1MB_WORKAROUND:
+            blobs = []
+            for i in range(BLOB_COUNT):
+                blob_cls = getattr(sys.modules[__name__],
+                                   'TaskResultBlob%d' % i)
+                blob = blob_cls.all().filter('task_result =', task_result).get()
+                if blob:
+                    blobs.append(blob)
+            db.delete(blobs)
+
+    @classmethod
+    def store_unsaved_content(cls, task_result):
+        if not hasattr(task_result, 'unsaved_content'):
+            app.logger.debug("Storing slices failed, no content found")
+            return
+
+        task_result.unsaved_content.seek(0)
+        for i in range(BLOB_COUNT):
+            app.logger.debug("Cheating AppEngine, storing blob slices...")
+            blob_cls = getattr(sys.modules[__name__], 'TaskResultBlob%d' % i)
+            blob = blob_cls(task_result=task_result,
+                            blob=db.Blob(task_result.unsaved_content.read(
+                                                                MAX_BLOB_SIZE)))
+            blob.put()
+        if len(task_result.unsaved_content.read(1)) > 0:
+            app.logger.debug("Storing slices failed, result too big")
+            raise ValueError("Result too big")
+
+    @property
+    def result(self):
+        buf = StringIO()
+        if USE_1MB_WORKAROUND:
+            for i in range(BLOB_COUNT):
+                blob_cls = getattr(sys.modules[__name__],
+                                   'TaskResultBlob%d' % i)
+                slice = blob_cls.all().filter('task_result =', self).get()
+                if slice:
+                    buf.write(slice.blob)
+                else:
+                    app.logger.debug("No slice %d" % i)
+            return buf.getvalue()
+        else:
+            return result_blob
+
+    @result.setter
+    def result(self, value):
+        app.logger.debug("Setting result with USE_1MB_WORKAROUND=%r" % USE_1MB_WORKAROUND)
+        if USE_1MB_WORKAROUND:
+            self.unsaved_content = StringIO(value)
+        else:
+            self.result_blob = db.Blob(value)
+
+
+class TaskResultBlob0(db.Model):
+    task_result = db.ReferenceProperty(TaskResult)
+    blob = db.BlobProperty()
+
+class TaskResultBlob1(db.Model):
+    task_result = db.ReferenceProperty(TaskResult)
+    blob = db.BlobProperty()
+
+class TaskResultBlob2(db.Model):
+    task_result = db.ReferenceProperty(TaskResult)
+    blob = db.BlobProperty()
+
+class TaskResultBlob3(db.Model):
+    task_result = db.ReferenceProperty(TaskResult)
+    blob = db.BlobProperty()
+
+class TaskResultBlob4(db.Model):
+    task_result = db.ReferenceProperty(TaskResult)
+    blob = db.BlobProperty()
+
+class TaskResultBlob5(db.Model):
+    task_result = db.ReferenceProperty(TaskResult)
+    blob = db.BlobProperty()
+
+class TaskResultBlob6(db.Model):
+    task_result = db.ReferenceProperty(TaskResult)
+    blob = db.BlobProperty()
+
+class TaskResultBlob7(db.Model):
+    task_result = db.ReferenceProperty(TaskResult)
+    blob = db.BlobProperty()
+
+class TaskResultBlob8(db.Model):
+    task_result = db.ReferenceProperty(TaskResult)
+    blob = db.BlobProperty()
+
+class TaskResultBlob9(db.Model):
+    task_result = db.ReferenceProperty(TaskResult)
+    blob = db.BlobProperty()
+
+BLOB_COUNT = 10
+
+
+tasks = {}
+"""Collects all tasks defined by the task decorator."""
+
+def task(func):
+    """Decorator for generating a task similar to Celery."""
+    @wraps(func, assigned=("__module__", "__name__"))
+    def run(self, *args, **kwargs):
+        return func(*args, **kwargs)
+
+    # TODO name should be set through class
+    #   (see metaclass in celery.task.base.TaskType)
+    name = func.__module__ + '.' + func.__name__ 
+    cls_dict = dict(run=run,
+                    name=name,
+                    __module__=func.__module__,
+                    __doc__=func.__doc__)
+    t = type(func.__name__, (GAETask, ), cls_dict)()
+    tasks[name] = t
+    return t
+
+
+class NotRegistered(Exception):
+    """Task is not registered."""
+
+
+class AsyncResult(object):
+    def __init__(self, task_id):
+        self._task_id = task_id
+
+    def _get_result_from_db(self):
+        if not hasattr(self, '_task_result'):
+            task_result = TaskResult.get_for_task_id(long(self._task_id))
+
+            if task_result is None:
+                raise NotRegistered()
+            if not task_result.available:
+                # TODO raise a better Exception
+                raise NotRegistered()
+
+            self._task_result = task_result
+
+        return self._task_result
+
+    def forget(self):
+        task_result = self._get_result_from_db()
+
+        task_result.available = False
+        TaskResult.add(task_result)
+
+    def ready(self):
+        task_result = self._get_result_from_db()
+        return not task_result.pending
+
+    def successful(self):
+        task_result = self._get_result_from_db()
+        return not task_result.pending and task_result.success
+
+    @property
+    def result(self):
+        task_result = self._get_result_from_db()
+        return pickle.loads(task_result.result)
+
+    @property
+    def task_id(self):
+        return str(self._task_id)
+
+def run(name, task_id, *args, **kwargs):
+    try:
+        output = tasks[name].run(*args, **kwargs)
+        success = True
+    except Exception, e:
+        app.logger.debug("Exception during run ... %r" % e)
+        output = e
+        success = False
+
+    try:
+        result = pickle.dumps(output)
+        task_result = TaskResult.get_for_task_id(task_id)
+        task_result.result = result
+        task_result.success = success
+        task_result.pending = False
+        TaskResult.add(task_result)
+    except Exception, e:
+        app.logger.debug("Exception storing result ... %r" % e)
+
+
+class GAETask(object):
+    def __call__(self, *args, **kwargs):
+        return self.run(*args, **kwargs)
+
+    def run(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    @classmethod
+    def delay(cls, *args, **kwargs):
+        return cls.apply_async(args, kwargs)
+
+    @classmethod
+    def apply_async(cls, args=None, kwargs=None, **options):
+        args = args or []
+        kwargs = kwargs or {}
+
+        # Create an empty result slot and yield id
+        task_result = TaskResult(result=None)
+        TaskResult.add(task_result)
+        task_id = task_result.task_id
+
+        # Run deferred
+        deferred.defer(run, cls.name, task_id, *args, **kwargs)
+
+        return cls.AsyncResult(task_id=task_id)
+
+    @classmethod
+    def AsyncResult(self, task_id):
+        return AsyncResult(task_id)

pdfserver/views.py
@@ -131,10 +131,10 @@ def combine_pdfs():
 
         return ((idx, files[idx-1]) for idx in order)
 
-    files = _get_uploads()
+    file_ids = session.get('file_ids', [])
 
     # If user clicked on button but no files were uploaded
-    if not files:
+    if not file_ids:
         return redirect(url_for('main'))
 
     # Get options
@@ -159,12 +159,12 @@ def combine_pdfs():
 
     # Get pdf objects and arrange in the user selected order, then get ranges
     order = request.form.getlist('file[]')
-    indices, uploads = zip(*order_files(files, order))
+    indices, file_ids = zip(*order_files(file_ids, order))
     pages = [request.form.get('pages_%d' % item_idx, "")
                     for item_idx in indices]
 
     # Do the actual work
-    resp = handle_pdfs_task.apply_async((uploads,
+    resp = handle_pdfs_task.apply_async((file_ids,
                                          pages,
                                          pages_sheet,
                                          rotate,

commit dbfbfa34eb96472c50f04568c3ccac145cfc528b

Fix ordering of uploads broken in 25629539d4bec796691b

pdfserver/tasks.py
@@ -17,7 +17,11 @@ elif app.config['TASK_HANDLER'] == 'pdfserver.gaetask':
 @task
 def handle_pdfs_task(file_ids, page_range_text=None, pages_sheet=1,
                      rotate=0, overlay=None):
-    files_handles = Upload.get_for_ids(file_ids)
+    uploads = Upload.get_for_ids(file_ids)
+    # Bring uploads into order specified by file_ids
+    uploads_map = dict((upload.id, upload) for upload in uploads)
+    files_handles = [uploads_map[id] for id in file_ids if id in uploads_map]
+
     output = handle_pdfs(files_handles, page_range_text=page_range_text,
                          pages_sheet=pages_sheet, rotate=rotate,
                          overlay=overlay)

pdfserver/tests.py
@@ -2,8 +2,10 @@ import unittest
 import os
 import re
 import difflib
+import random
 from StringIO import StringIO
 
+import werkzeug
 from flask import session
 
 from pyPdf import PdfFileReader, PdfFileWriter
@@ -422,6 +424,15 @@ class CombineTestCase(DownloadMixin, PdfserverTestCase):
         new_page[NameObject('/Contents')] = content
         return new_page
 
+    @classmethod
+    def clone_document(cls, pdf, text, replace):
+        output = PdfFileWriter()
+        for page_idx in range(pdf.getNumPages()):
+            page = pdf.getPage(page_idx)
+            new_page = cls.replace_text(page, text, replace)
+            output.addPage(new_page)
+        return output
+
     def test_page_ranges(self):
         pdf = PdfFileReader(self.get_pdf_stream())
 
@@ -452,6 +463,42 @@ class CombineTestCase(DownloadMixin, PdfserverTestCase):
                         for i, page in enumerate(ranges)))
 
         self.assertEquals(pdf_download.getNumPages(), len(ranges))
+
+        self.clean_up()
+
+    def test_file_order(self):
+        pdf = PdfFileReader(self.get_pdf_stream())
+
+        with self.app as c:
+            # Upload 20 documents
+            order = range(1, 20)
+            for i in order:
+                new_pdf = CombineTestCase.clone_document(pdf, 'Test',
+                                                         'Test %d' % i)
+
+                buf = StringIO()
+                new_pdf.write(buf)
+                buf.seek(0)
+
+                c.post('/upload', data={'file': (buf, 'test_%d.pdf' % i)})
+
+            # Re-arrange uploads and download combined files
+            random.shuffle(order)
+
+            file_ids = session['file_ids']
+            options = werkzeug.MultiDict([('file[]', i) for i in order]
+                                        + [('upload_id_%d' % j, id)
+                                            for j, id in enumerate(file_ids)])
+            rv = self.combine_and_download(**options)
+
+        pdf_download = PdfFileReader(StringIO(rv.data))
+
+        # Test order
+        self.assert_(all(
+                    (('Test %d' % idx) in pdf_download.getPage(i).extractText())
+                    for i, idx in enumerate(order)))
+
+        self.assertEquals(pdf_download.getNumPages(), len(order))
 
         self.clean_up()
 