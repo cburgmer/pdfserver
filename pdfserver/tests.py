cburgmer/pdfserver

commit 38f8e18007c5aba3b8279059b28ab0d4f8bd2e24

Handle browsers without cookies gracefully

pdfserver/views.py
@@ -4,6 +4,7 @@ from operator import attrgetter
 
 from flask import g, request, Response, session, render_template
 from flask import abort, redirect, url_for, jsonify
+from flaskext.babel import gettext
 from werkzeug import wrap_file
 from werkzeug.exceptions import InternalServerError, MethodNotAllowed, Gone, \
                                 NotFound
@@ -78,6 +79,7 @@ def _order_files(files):
 @templated('main.html')
 def main():
     files = _get_uploads()
+    session['has_cookies'] = 1
     return {'uploads': files}
 
 def main_table():
@@ -104,6 +106,12 @@ def handle_form():
         raise MethodNotAllowed()
 
 def upload_file():
+    if not session.get('has_cookies', 0) == 1:
+        return Response('<html><body><span id="cookies">'
+                        + gettext('Please activate cookies '
+                                  'so your uploads can be linked to you.')
+                        + '</span></body></html>')
+
     if 'file' in request.files and request.files['file']:
         app.logger.info("Upload form is valid")
         upload = Upload()
@@ -123,10 +131,7 @@ def upload_file():
     else:
         app.logger.error("No file specified")
 
-    if request.is_xhr:
-        return ''
-    else:
-        return redirect(url_for('main'))
+    return redirect(url_for('main'))
 
 #@templated('confirm_delete.html')
 #def confirm_delete():

TODO
@@ -1,4 +1,3 @@
-* Warn that cookies need to be activated.
 * Clean up expired sessions & files from these sessions (use CRON).
 * Only show options that are actually available (dependencies!).
 * Expire results from celery (using TASK_RESULT_EXPIRES)

pdfserver/templates/main.html
@@ -62,6 +62,9 @@
                         $.jnotify('{{ _("Error updating file list: ") }}' + st, "error", true);
                     },
                 });
+            } else if (window.frames["upload_target"].document.getElementById('cookies')) {
+                $("#upload-progress").css('visibility', 'hidden');
+                $.jnotify('{{ _("Please activate cookies so your uploads can be linked to you.") }}', "error", true);
             } else {
                 $("#upload-progress").css('visibility', 'hidden');
                 $.jnotify('{{ _("Unable to upload file ") }}', "error", true);

commit 3c87d64538d4bdf7da52ab658c4ec43803562705

Fix testing broken in 38f8e18007c5aba3b827

pdfserver/tests.py
@@ -70,6 +70,9 @@ class PdfserverTestCase(unittest.TestCase):
 class UploadTestCase(PdfserverTestCase):
 
     def test_upload_returns_redirect(self):
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')})
 
@@ -78,6 +81,9 @@ class UploadTestCase(PdfserverTestCase):
         self.clean_up()
 
     def test_upload_shows_resulting_file(self):
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')},
                            follow_redirects=True)
@@ -88,6 +94,9 @@ class UploadTestCase(PdfserverTestCase):
 
     def test_upload_creates_file(self):
         from pdfserver.models import Upload
+
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
 
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')})
@@ -102,6 +111,9 @@ class UploadTestCase(PdfserverTestCase):
 
         assert Upload.query.count() == 0
 
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')})
 
@@ -113,6 +125,9 @@ class UploadTestCase(PdfserverTestCase):
         from pdfserver.models import Upload
 
         assert Upload.query.count() == 0
+
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
 
         with self.app as c:
             rv = c.post('/handleform',
@@ -141,6 +156,9 @@ class DeleteTestCase(PdfserverTestCase):
         from pdfserver.models import Upload
         assert Upload.query.count() == 0
 
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')},
                            follow_redirects=True)
@@ -161,6 +179,9 @@ class DeleteTestCase(PdfserverTestCase):
         from pdfserver.models import Upload
         assert Upload.query.count() == 0
 
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         self.app.post('/handleform',
                       data={'file': (self.get_pdf_stream(), 'test.pdf')})
         self.app.post('/handleform',
@@ -179,6 +200,9 @@ class DeleteTestCase(PdfserverTestCase):
     def test_remove_does_not_affect_others(self):
         from pdfserver.models import Upload
         assert Upload.query.count() == 0
+
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
 
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')},
@@ -215,6 +239,9 @@ class DeleteTestCase(PdfserverTestCase):
         from pdfserver.models import Upload
         assert Upload.query.count() == 0
 
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         rv = self.app.post('/delete',
                            data={'delete': 'delete', 'id': '1'},
                            follow_redirects=True)
@@ -224,6 +251,9 @@ class DeleteTestCase(PdfserverTestCase):
     def test_confirm_delete_cancel_has_no_effect(self):
         from pdfserver.models import Upload
         assert Upload.query.count() == 0
+
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
 
         self.app.post('/handleform',
                       data={'file': (self.get_pdf_stream(), 'test.pdf')})
@@ -251,11 +281,17 @@ class InteractionTestCase(PdfserverTestCase):
         from pdfserver.models import Upload
         assert Upload.query.count() == 0
 
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         self.app.post('/handleform',
                       data={'file': (self.get_pdf_stream(), 'test.pdf')})
         ids = [upload.id for upload in Upload.query.all()]
         self.assertEquals(len(ids), 1)
         app_id = ids[0]
+
+        rv = self.app2.get('/')
+        self.assertEquals(rv.status_code, 200)
 
         self.app2.post('/handleform',
                        data={'file': (self.get_pdf_stream(), 'test.pdf')})
@@ -282,11 +318,17 @@ class InteractionTestCase(PdfserverTestCase):
         from pdfserver.models import Upload
         assert Upload.query.count() == 0
 
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         self.app.post('/handleform',
                       data={'file': (self.get_pdf_stream(), 'test.pdf')})
         ids = [upload.id for upload in Upload.query.all()]
         self.assertEquals(len(ids), 1)
         app_id = ids[0]
+
+        rv = self.app2.get('/')
+        self.assertEquals(rv.status_code, 200)
 
         self.app2.post('/handleform',
                        data={'file': (self.get_pdf_stream(), 'test.pdf')})
@@ -342,6 +384,9 @@ class DownloadTestCase(DownloadMixin, PdfserverTestCase):
         pdf = PdfFileReader(self.get_pdf_stream())
         assert 'Test' in pdf.getPage(0).extractText()
 
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')})
 
@@ -358,6 +403,9 @@ class DownloadTestCase(DownloadMixin, PdfserverTestCase):
         from pdfserver.faketask import TaskResult
         assert TaskResult.query.count() == 0
 
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         rv = self.app.get('/download/1')
 
         self.assertEquals(rv.status_code, 404)
@@ -365,6 +413,9 @@ class DownloadTestCase(DownloadMixin, PdfserverTestCase):
         self.clean_up()
 
     def test_download_removed_fails(self):
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')})
 
@@ -395,6 +446,9 @@ class DownloadTestCase(DownloadMixin, PdfserverTestCase):
     def test_download_expired_fails(self):
         from pdfserver.faketask import TaskResult
         assert TaskResult.query.count() == 0
+
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
 
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')})
@@ -450,6 +504,9 @@ class CombineTestCase(DownloadMixin, PdfserverTestCase):
         return output
 
     def test_page_ranges(self):
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         pdf = PdfFileReader(self.get_pdf_stream())
 
         # Build a document with twenty pages
@@ -486,6 +543,9 @@ class CombineTestCase(DownloadMixin, PdfserverTestCase):
         self.clean_up()
 
     def test_file_order(self):
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         pdf = PdfFileReader(self.get_pdf_stream())
 
         with self.app as c:
@@ -526,6 +586,9 @@ class WatermarkTestCase(DownloadMixin, PdfserverTestCase):
 
     def setUp(self):
         super(WatermarkTestCase, self).setUp()
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')})
 
@@ -551,6 +614,9 @@ class RotationTestCase(DownloadMixin, PdfserverTestCase):
         pdf = PdfFileReader(self.get_pdf_stream())
         assert 'Test' in pdf.getPage(0).extractText()
 
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')})
 
@@ -565,6 +631,9 @@ class RotationTestCase(DownloadMixin, PdfserverTestCase):
         # Make sure our Test string is available in the original document
         pdf = PdfFileReader(self.get_pdf_stream())
         assert 'Test' in pdf.getPage(0).extractText()
+
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
 
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')})
@@ -585,6 +654,9 @@ class RotationTestCase(DownloadMixin, PdfserverTestCase):
         # Make sure our Test string is available in the original document
         pdf = PdfFileReader(self.get_pdf_stream())
         assert 'Test' in pdf.getPage(0).extractText()
+
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
 
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')})
@@ -622,6 +694,9 @@ class NPagesTestCase(DownloadMixin, PdfserverTestCase):
         assert 'Test' in pdf.getPage(0).extractText()
         assert pdf.getNumPages() == 1
 
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')})
 
@@ -645,6 +720,9 @@ class NPagesTestCase(DownloadMixin, PdfserverTestCase):
         output.write(buf)
         buf.seek(0)
 
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
+
         rv = self.app.post('/handleform',
                            data={'file': (buf, 'test.pdf')})
 
@@ -662,6 +740,9 @@ class NPagesTestCase(DownloadMixin, PdfserverTestCase):
         pdf = PdfFileReader(self.get_pdf_stream())
         assert 'Test' in pdf.getPage(0).extractText()
         assert pdf.getNumPages() == 1
+
+        rv = self.app.get('/')
+        self.assertEquals(rv.status_code, 200)
 
         rv = self.app.post('/handleform',
                            data={'file': (self.get_pdf_stream(), 'test.pdf')})

pdfserver/views.py
@@ -107,6 +107,7 @@ def handle_form():
 
 def upload_file():
     if not session.get('has_cookies', 0) == 1:
+        app.logger.debug("No cookie found")
         return Response('<html><body><span id="cookies">'
                         + gettext('Please activate cookies '
                                   'so your uploads can be linked to you.')